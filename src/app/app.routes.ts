ce1e57d8cee1f0ae906f00c10228ab3f4141115b
Claus <tab88fn@gmail.com>
2025-05-19 20:08:53 -0600

    landing page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { GeographyComponent } from './geography/geography.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent
+  },
+  {
+    path: 'geography',
+    component: GeographyComponent,
+  },
+];
---
+++ src/app/geography/geography.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-geography',
+  imports: [],
+  templateUrl: './geography.component.html',
+  styleUrl: './geography.component.css'
+})
+export class GeographyComponent {
+
+}
